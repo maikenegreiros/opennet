06e5d7d58df1dc498b7192d81ad169fa67d4245c
Maike Negreiros <maike_jbe@hotmail.com>
2019-05-08 19:38:46 -0300

    Updates

---
+++ precache-manifest.064bf902935d8cdfd8dfe077581978a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5984b0e1ab2bf3cb74e2567076bcf943",
+    "url": "/opennet/index.html"
+  },
+  {
+    "revision": "8d15f8e1b595a51f00b5",
+    "url": "/opennet/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8b1030fc732dd0cfbc69",
+    "url": "/opennet/static/css/main.568b1761.chunk.css"
+  },
+  {
+    "revision": "8d15f8e1b595a51f00b5",
+    "url": "/opennet/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "8b1030fc732dd0cfbc69",
+    "url": "/opennet/static/js/main.4c268ffb.chunk.js"
+  },
+  {
+    "revision": "4d93b06b3a22be727fe8",
+    "url": "/opennet/static/js/runtime~main.5a1f7704.js"
+  }
+]);
