3b21d21e3121ffdf2e8b88cb1decf2295c1f0d8c
Maike Negreiros <maike_jbe@hotmail.com>
2019-05-08 19:25:07 -0300

    Build

---
+++ build/precache-manifest.bdd381e33a56133d5a10b33712a76787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d6fbdab195ce4726ead87eae215c59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d15f8e1b595a51f00b5",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c67d127e96aeb295cfaf",
+    "url": "/static/css/main.568b1761.chunk.css"
+  },
+  {
+    "revision": "8d15f8e1b595a51f00b5",
+    "url": "/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "c67d127e96aeb295cfaf",
+    "url": "/static/js/main.50e91057.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
